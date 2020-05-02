fe1d57b7fb5ce02d11eda94e8b6e874186c55c49
GustavoDuregger <gustavo.duregger@gmail.com>
2020-05-02 17:32:26 -0300

    Games Bar select

--- script/script.js
+++ script/script.js
@@ -3,6 +3,10 @@ function togglePopup4() {
         document.getElementById('btn4Text').style.display = 'none';
     }else {
         document.getElementById('btn4Text').style.display = 'block';
+        document.getElementById('btn1Text').style.display = 'none';
+        document.getElementById('btn2Text').style.display = 'none';
+        document.getElementById('btn3Text').style.display = 'none';
+        document.getElementById('btn5Text').style.display = 'none';
     }
 }
 
@@ -11,6 +15,10 @@ function togglePopup3() {
         document.getElementById('btn3Text').style.display = 'none';
     }else {
         document.getElementById('btn3Text').style.display = 'block';
+        document.getElementById('btn1Text').style.display = 'none';
+        document.getElementById('btn2Text').style.display = 'none';
+        document.getElementById('btn4Text').style.display = 'none';
+        document.getElementById('btn5Text').style.display = 'none';
     }
 }
 
@@ -19,6 +27,10 @@ function togglePopup1() {
         document.getElementById('btn1Text').style.display = 'none';
     }else {
         document.getElementById('btn1Text').style.display = 'block';
+        document.getElementById('btn4Text').style.display = 'none';
+        document.getElementById('btn2Text').style.display = 'none';
+        document.getElementById('btn3Text').style.display = 'none';
+        document.getElementById('btn5Text').style.display = 'none';
     }
 }
 
@@ -27,6 +39,10 @@ function togglePopup2() {
         document.getElementById('btn2Text').style.display = 'none';
     }else {
         document.getElementById('btn2Text').style.display = 'block';
+        document.getElementById('btn1Text').style.display = 'none';
+        document.getElementById('btn4Text').style.display = 'none';
+        document.getElementById('btn3Text').style.display = 'none';
+        document.getElementById('btn5Text').style.display = 'none';
     }
 }
 
@@ -35,7 +51,123 @@ function togglePopup5() {
         document.getElementById('btn5Text').style.display = 'none';
     }else {
         document.getElementById('btn5Text').style.display = 'block';
+        document.getElementById('btn1Text').style.display = 'none';
+        document.getElementById('btn2Text').style.display = 'none';
+        document.getElementById('btn3Text').style.display = 'none';
+        document.getElementById('btn4Text').style.display = 'none';
     }
 }
 
+function togglePopupG1() {
+    if (document.getElementById('gameBar1').style.display == 'block') {
+        document.getElementById('gameBar1').style.display = 'none';
+    }else {
+        document.getElementById('gameBar1').style.display = 'block';
+        document.getElementById('gameBar2').style.display = 'none';
+        document.getElementById('gameBar3').style.display = 'none';
+        document.getElementById('gameBar4').style.display = 'none';
+        document.getElementById('gameBar5').style.display = 'none';
+        document.getElementById('gameBar6').style.display = 'none';
+        document.getElementById('gameBar7').style.display = 'none';
+        document.getElementById('gameBar8').style.display = 'none';
+    }
+}
+function togglePopupG2() {
+    if (document.getElementById('gameBar2').style.display == 'block') {
+        document.getElementById('gameBar2').style.display = 'none';
+    }else {
+        document.getElementById('gameBar2').style.display = 'block';
+        document.getElementById('gameBar1').style.display = 'none';
+        document.getElementById('gameBar3').style.display = 'none';
+        document.getElementById('gameBar4').style.display = 'none';
+        document.getElementById('gameBar5').style.display = 'none';
+        document.getElementById('gameBar6').style.display = 'none';
+        document.getElementById('gameBar7').style.display = 'none';
+        document.getElementById('gameBar8').style.display = 'none';
+    }
+}
+function togglePopupG3() {
+    if (document.getElementById('gameBar3').style.display == 'block') {
+        document.getElementById('gameBar3').style.display = 'none';
+    }else {
+        document.getElementById('gameBar3').style.display = 'block';
+        document.getElementById('gameBar2').style.display = 'none';
+        document.getElementById('gameBar1').style.display = 'none';
+        document.getElementById('gameBar4').style.display = 'none';
+        document.getElementById('gameBar5').style.display = 'none';
+        document.getElementById('gameBar6').style.display = 'none';
+        document.getElementById('gameBar7').style.display = 'none';
+        document.getElementById('gameBar8').style.display = 'none';
+    }
+}
+function togglePopupG4() {
+    if (document.getElementById('gameBar4').style.display == 'block') {
+        document.getElementById('gameBar4').style.display = 'none';
+    }else {
+        document.getElementById('gameBar4').style.display = 'block';
+        document.getElementById('gameBar2').style.display = 'none';
+        document.getElementById('gameBar1').style.display = 'none';
+        document.getElementById('gameBar3').style.display = 'none';
+        document.getElementById('gameBar5').style.display = 'none';
+        document.getElementById('gameBar6').style.display = 'none';
+        document.getElementById('gameBar7').style.display = 'none';
+        document.getElementById('gameBar8').style.display = 'none';
+    }
+}
+function togglePopupG5() {
+    if (document.getElementById('gameBar5').style.display == 'block') {
+        document.getElementById('gameBar5').style.display = 'none';
+    }else {
+        document.getElementById('gameBar5').style.display = 'block';
+        document.getElementById('gameBar2').style.display = 'none';
+        document.getElementById('gameBar1').style.display = 'none';
+        document.getElementById('gameBar4').style.display = 'none';
+        document.getElementById('gameBar3').style.display = 'none';
+        document.getElementById('gameBar6').style.display = 'none';
+        document.getElementById('gameBar7').style.display = 'none';
+        document.getElementById('gameBar8').style.display = 'none';
+    }
+}
+function togglePopupG6() {
+    if (document.getElementById('gameBar6').style.display == 'block') {
+        document.getElementById('gameBar6').style.display = 'none';
+    }else {
+        document.getElementById('gameBar6').style.display = 'block';
+        document.getElementById('gameBar2').style.display = 'none';
+        document.getElementById('gameBar1').style.display = 'none';
+        document.getElementById('gameBar3').style.display = 'none';
+        document.getElementById('gameBar5').style.display = 'none';
+        document.getElementById('gameBar4').style.display = 'none';
+        document.getElementById('gameBar7').style.display = 'none';
+        document.getElementById('gameBar8').style.display = 'none';
+    }
+}
+function togglePopupG7() {
+    if (document.getElementById('gameBar7').style.display == 'block') {
+        document.getElementById('gameBar7').style.display = 'none';
+    }else {
+        document.getElementById('gameBar7').style.display = 'block';
+        document.getElementById('gameBar2').style.display = 'none';
+        document.getElementById('gameBar1').style.display = 'none';
+        document.getElementById('gameBar4').style.display = 'none';
+        document.getElementById('gameBar5').style.display = 'none';
+        document.getElementById('gameBar6').style.display = 'none';
+        document.getElementById('gameBar3').style.display = 'none';
+        document.getElementById('gameBar8').style.display = 'none';
+    }
+}
+function togglePopupG8() {
+    if (document.getElementById('gameBar8').style.display == 'block') {
+        document.getElementById('gameBar8').style.display = 'none';
+    }else {
+        document.getElementById('gameBar8').style.display = 'block';
+        document.getElementById('gameBar2').style.display = 'none';
+        document.getElementById('gameBar1').style.display = 'none';
+        document.getElementById('gameBar4').style.display = 'none';
+        document.getElementById('gameBar5').style.display = 'none';
+        document.getElementById('gameBar6').style.display = 'none';
+        document.getElementById('gameBar7').style.display = 'none';
+        document.getElementById('gameBar3').style.display = 'none';
+    }
+}
 
