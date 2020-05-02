42528e90cc7d1982c53fd2ca33f38443ab8d9459
GustavoDuregger <gustavo.duregger@gmail.com>
2020-05-02 15:17:08 -0300

    btn functions

--- script/script.js
+++ script/script.js
@@ -1,13 +1,41 @@
-function displayBtn()
-{
-    var btn = document.getElementById("btn");
-
-    if(document.getElementById("interadir").checked)
-    {
-        btn.style.display = "none";
-    }
-    else
-    {
-        btn.style.display = "block";
+function togglePopup4() {
+    if (document.getElementById('btn4Text').style.display == 'block') {
+        document.getElementById('btn4Text').style.display = 'none';
+    }else {
+        document.getElementById('btn4Text').style.display = 'block';
     }
 }
+
+function togglePopup3() {
+    if (document.getElementById('btn3Text').style.display == 'block') {
+        document.getElementById('btn3Text').style.display = 'none';
+    }else {
+        document.getElementById('btn3Text').style.display = 'block';
+    }
+}
+
+function togglePopup1() {
+    if (document.getElementById('btn1Text').style.display == 'block') {
+        document.getElementById('btn1Text').style.display = 'none';
+    }else {
+        document.getElementById('btn1Text').style.display = 'block';
+    }
+}
+
+function togglePopup2() {
+    if (document.getElementById('btn2Text').style.display == 'block') {
+        document.getElementById('btn2Text').style.display = 'none';
+    }else {
+        document.getElementById('btn2Text').style.display = 'block';
+    }
+}
+
+function togglePopup5() {
+    if (document.getElementById('btn5Text').style.display == 'block') {
+        document.getElementById('btn5Text').style.display = 'none';
+    }else {
+        document.getElementById('btn5Text').style.display = 'block';
+    }
+}
+
+
